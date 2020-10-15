a3eb8ef2d5a576ef14fd57c460a0ad87a45ef06b
TomorrowCat <1002563923@qq.com>
2020-10-15 10:53:13 +0800

    entity增加swagger竹节

--- src/decorator/column.decorator.ts
+++ src/decorator/column.decorator.ts
@@ -3,46 +3,131 @@ import {
   ENTITY_CREATED_USER_COLUMN,
   ENTITY_UPDATED_USER_COLUMN,
   ENTITY_SOFT_DELETE_COLUMN,
-} from './decorator.constant';
+} from "./decorator.constant";
+import { ApiProperty } from "@nestjs/swagger";
+import { Column as TypeormColumn, ColumnOptions } from "typeorm";
+import { ColumnCommonOptions } from "typeorm/decorator/options/ColumnCommonOptions";
+import { ColumnWithWidthOptions } from "typeorm/decorator/options/ColumnWithWidthOptions";
+import { SpatialColumnOptions } from "typeorm/decorator/options/SpatialColumnOptions";
+import { ColumnWithLengthOptions } from "typeorm/decorator/options/ColumnWithLengthOptions";
+import { ColumnNumericOptions } from "typeorm/decorator/options/ColumnNumericOptions";
+import { ColumnEnumOptions } from "typeorm/decorator/options/ColumnEnumOptions";
+import { ColumnHstoreOptions } from "typeorm/decorator/options/ColumnHstoreOptions";
+import { ColumnEmbeddedOptions } from "typeorm/decorator/options/ColumnEmbeddedOptions";
+import {
+  ColumnType,
+  SimpleColumnType,
+  SpatialColumnType,
+  WithLengthColumnType,
+  WithPrecisionColumnType,
+  WithWidthColumnType,
+} from "typeorm/driver/types/ColumnTypes";
+
+export function Column(): PropertyDecorator;
+export function Column(options: ColumnOptions): PropertyDecorator;
+export function Column(
+  type: SimpleColumnType,
+  options?: ColumnCommonOptions
+): PropertyDecorator;
+export function Column(
+  type: SpatialColumnType,
+  options?: ColumnCommonOptions & SpatialColumnOptions
+): PropertyDecorator;
+export function Column(
+  type: WithLengthColumnType,
+  options?: ColumnCommonOptions & ColumnWithLengthOptions
+): PropertyDecorator;
+export function Column(
+  type: WithWidthColumnType,
+  options?: ColumnCommonOptions & ColumnWithWidthOptions
+): PropertyDecorator;
+export function Column(
+  type: WithPrecisionColumnType,
+  options?: ColumnCommonOptions & ColumnNumericOptions
+): PropertyDecorator;
+export function Column(
+  type: "enum",
+  options?: ColumnCommonOptions & ColumnEnumOptions
+): PropertyDecorator;
+export function Column(
+  type: "simple-enum",
+  options?: ColumnCommonOptions & ColumnEnumOptions
+): PropertyDecorator;
+export function Column(
+  type: "set",
+  options?: ColumnCommonOptions & ColumnEnumOptions
+): PropertyDecorator;
+export function Column(
+  type: "hstore",
+  options?: ColumnCommonOptions & ColumnHstoreOptions
+): PropertyDecorator;
+export function Column(
+  type: (type?: any) => Function,
+  options?: ColumnEmbeddedOptions
+): PropertyDecorator;
+export function Column(
+  typeOrOptions?:
+    | ((type?: any) => Function)
+    | ColumnType
+    | (ColumnOptions & ColumnEmbeddedOptions),
+  options?: ColumnOptions & ColumnEmbeddedOptions
+): PropertyDecorator {
+  return function (object: Object, propertyName: string) {
+    // @ts-ignore
+    TypeormColumn(typeOrOptions, options)(object, propertyName);
+    let desc;
+    // @ts-ignore
+    if (typeOrOptions && typeOrOptions.comment) {
+      // @ts-ignore
+      desc = typeOrOptions.comment;
+    } else if (options && options.comment) {
+      desc = options.comment;
+    }
+    desc &&
+      ApiProperty({
+        description: desc,
+      })(object, propertyName);
+  };
+}
 
 export function IdColumn(value?: string): Function {
-  return function(object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string) {
     Reflect.defineMetadata(
       ENTITY_ID_COLUMN,
       value || propertyName,
-      object.constructor,
+      object.constructor
     );
   };
 }
 
 export function CreateUserColumn(value?: string): Function {
-  return function(object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string) {
     Reflect.defineMetadata(
       ENTITY_CREATED_USER_COLUMN,
       value || propertyName,
-      object.constructor,
+      object.constructor
     );
   };
 }
 
 export function UpdateUserColumn(value?: string): Function {
-  return function(object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string) {
     Reflect.defineMetadata(
       ENTITY_UPDATED_USER_COLUMN,
       value || propertyName,
-      object.constructor,
+      object.constructor
     );
   };
 }
 
 export function SoftDeleteColumn(value: number | string): Function {
-  return function(object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string) {
     Reflect.defineMetadata(
       ENTITY_SOFT_DELETE_COLUMN,
       {
         [propertyName]: value,
       },
-      object.constructor,
+      object.constructor
     );
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-export * from 'typeorm';
-export * from '@nestjs/typeorm';
-export * from './orm.module';
-export * from './orm.logger';
-export * from './orm.provider';
-export * from './orm.interface';
-export * from './snake_naming.strategy';
-export * from './basic.service';
-export * from './basic.entity';
-export * from './decorator';
-export * from './scope';
-export * from './operator';
+export * from "@nestjs/typeorm";
+export * from "./orm.module";
+export * from "./orm.logger";
+export * from "./orm.provider";
+export * from "./orm.interface";
+export * from "./snake_naming.strategy";
+export * from "./basic.service";
+export * from "./basic.entity";
+export * from "./decorator";
+export * from "./scope";
+export * from "./operator";
+// @ts-ignore
+export * from "typeorm";
 
-import { BasicService, Service } from './basic.service';
-import { Repository } from 'typeorm';
+import { BasicService, Service } from "./basic.service";
+import { Repository } from "typeorm";
 
-export function getService<T, S extends Service<T> = Service<T>>(repository: Repository<T>) {
+export function getService<T, S extends Service<T> = Service<T>>(
+  repository: Repository<T>
+) {
   return BasicService.getService<T, S>(repository);
 }
